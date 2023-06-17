e6ab47fd9aee56a78f810a1535632c2ef1e39dd5
Pratham <prathamsinghal2003@gmail.com>
2023-06-17 19:32:19 +0530

    feat: add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from './typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DB_NAME,
+      entities,
+      synchronize: true,
+    }),
+  ],
+
+  // host: 'localhost',
+
   controllers: [],
   providers: [],
 })
